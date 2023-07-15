a80745ff9d92f21920254f478627a42d10d54152
Paola Beltran <pavis.asajfb@gmail.com>
2023-07-14 23:36:15 -0500

    primer intento cajero

---
+++ script.js
@@ -0,0 +1,22 @@
+function borrarTexto(input) {
+    if (input.value === "Número") {
+      input.value = "";
+    }
+  }
+
+  function borrarTexto1(input) {
+    if (input.value === "Contraseña") {
+      input.value = "";
+    }
+  }
+/*
+  let selectElement = document.getElementById("tipoDoc");
+  let valorSeleccionado = selectElement.value;
+  console.log(valorSeleccionado);
+ 
+  var cuentas = [
+    { nombre: “Mali”, saldo: 200 }
+    { nombre: “Gera”, saldo: 290 }
+    { nombre: “Maui”, saldo: 67 }
+  ];
+   */
